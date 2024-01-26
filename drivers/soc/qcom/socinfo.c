5824458cd82aba8a5378b5dc6ad6aad6183647c2
QKIvan <ivan33@163.com>
2024-01-26 11:33:08 +0000

    redbull: Revert multiple commits
    - These commits make cpu times in high MHz,revert them to get better battery time.
    - Revert configs for APatch,We already have KernelSU version,so we don't need it.

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -1881,7 +1881,7 @@ int __init socinfo_init(void)
 
 	return 0;
 }
-device_initcall(socinfo_init);
+subsys_initcall(socinfo_init);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Qualcomm Socinfo driver");
